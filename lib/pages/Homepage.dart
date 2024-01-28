80035de3d7f531499ae465d030325aa919501528
Adrian Kim D. Requillo <Akrequillo@gmail.com>
2024-01-28 14:16:38 +1000

    first commit

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -1,4 +1,7 @@
+import 'package:adaptive_navbar/adaptive_navbar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:renote/pages/Profile.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,9 +13,40 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
+    final sw = MediaQuery.of(context).size.width;
 
-    
     return Scaffold(
+      appBar: AdaptiveNavBar(
+        automaticallyImplyLeading: false,
+        screenWidth: sw,
+        title: RichText(
+          text: TextSpan(
+            children: [
+              WidgetSpan(
+                  child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                // child: Container(
+                //     height: 70, child: Image.asset('lib/assets/images/J.png')),
+              )),
+            ],
+          ),
+        ),
+        navBarItems: [
+          NavBarItem(
+            text: "Profile",
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => profile()));
+            },
+          ),
+          NavBarItem(
+            text: "Log out",
+            onTap: () {
+              FirebaseAuth.instance.signOut();
+            },
+          )
+        ],
+      ),
       body: Container(
         child: Text("Homepage"),
       ),
